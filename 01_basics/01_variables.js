12a27a206e020697667022c76dfe70f46ef936b6
Ajeet Kumar Tiwari <119010755+ajeet8827@users.noreply.github.com>
2024-12-04 18:12:59 +0000

    variable and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14566
+let accountEmail ="sanju@gmail.com"
+var accountPassword ="1234"
+accountCity ="bhopal"
+//accountId=432 //not allowed
+accountEmail="sujeet@gmail.com"
+accountPassword="222"
+accountCity="rewa"
+console.log(accountId);
+/*
+prefer not to use var because of issue in block 
+scope and functional scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"// treat all Js code as newer version 
+//alert(3 + 3)// we are using nodejs , not browser
+
+console.log(3
+      +  3
+)// code readability should be high....
+
+console.log("sanju");
+let name = "king kohli"
+let age =36
+let isLoggedIn =false
+//let state; => only declare the variable not assign is called => undefined
+
+let state=null;// we can say => empty
+
+//number =>2 to power 53
+// bigInt
+//string => ""
+//boolean => true / false
+// null => standlone value
+// undefined => value not assign (samjhee)
+
+console.log(typeof "sanju");
+console.log(typeof age)
+console.log(typeof null)// => object
+console.log(typeof undefined)// => undefined
+
+// command for run code on terminal-:
+// node 01_basics/02_dataTypes.js =>then enter
