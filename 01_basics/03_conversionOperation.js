e9c37673451e4a10ec7437baf4b218635c283e06
Ajeet Kumar Tiwari <119010755+ajeet8827@users.noreply.github.com>
2024-12-12 18:36:50 +0000

    Operations Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,17 +16,17 @@ let valueInNumber = Number(score1)
 // ***FOR STRING CONVERSION ***
 
 let name ="sanju"
-console.log(name)
+// console.log(name)
 let convertInNum = Number(name)
-console.log(convertInNum)// Nan
-console.log(typeof convertInNum)// type => number...
+// console.log(convertInNum)// Nan
+// console.log(typeof convertInNum)// type => number...
 
 // CONVERSION OF 1 OR 0 TO BOOLEAN LETS CHECK KRTE HAI...
 
 let isLoggedIn = 0 //1
-console.log(typeof isLoggedIn)
+// console.log(typeof isLoggedIn)
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //           ***note***
 /*
@@ -39,3 +39,44 @@ console.log(booleanIsLoggedIn)
           "sanju" => true
 
 */
+
+
+// ******************Operations**********************
+let value =5
+let negValue= -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "ajeet"
+let str2 = " tiwari"
+let str = (str1 + str2)
+// console.log(str);
+
+// console.log("1" + 1)
+// console.log(1 + "1");
+// console.log("1" + 2 + 2);
+// console.log(1 + 1 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
