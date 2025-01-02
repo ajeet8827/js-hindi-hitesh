e60e9ac5636072baea63481cdb7b78e77a42577f
Ajeet Kumar Tiwari <119010755+ajeet8827@users.noreply.github.com>
2025-01-02 22:28:39 +0000

    comparison done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// basic 
+console.log(3>2);
+console.log(23<=9);
+// tricky
+console.log("2">1);
+console.log("02">2);
+
+// console.log("hello jee kaise ho sare")
+// console.log(null>0);
+// console.log(null<=0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (strict)
+console.log("2" === 2);
+
+
+
+
+
